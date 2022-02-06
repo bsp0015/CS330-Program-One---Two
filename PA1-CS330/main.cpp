0a80d0ce3fd7ba68608700dc1d17f6abeeb88e82
Brandon Perry <bsp0015@uah.edu>
2022-02-05 19:08:36 -0600

    Initial Commit

---
+++ PA1-CS330/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PA1-CS330
+//
+//  Created by Brandon Perry on 2/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
